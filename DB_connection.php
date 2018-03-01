3f1b8193ec843a3a96415fad116e3cb97ab88603
Pranshu Jethmalani <pmj642@gmail.com>
2018-03-01 15:53:52 +0530

    Added question template and database connection

---
+++ DB_connection.php
@@ -0,0 +1,40 @@
+<?php
+
+    $con = new mysqli("localhost","root","","oj");
+
+    if($con->connect_error)
+    {
+        die("Failed to connect to database! ".$con->connect_error);
+    }
+
+    // echo "Connected to database successfully<br>";
+    // echo "Query successful";
+
+    $con->set_charset("utf8");
+    $sql = "select * from questions";
+    $result = $con->query($sql);
+    // echo "Query successful<br>";
+
+    $row = $result->fetch_assoc();
+    echo "<html>
+            <head>
+                <title>".$row["name"]."</title>
+                <meta charset='utf-8'>
+            </head>
+            <body>";
+    // if($result->num_rows>0) {
+        // while() {
+            echo "name: ".$row["name"]."<br>";
+            $cons = $con->real_escape_string($row["constraints"]);
+            echo nl2br($cons)."<br>";
+        // }
+    // }
+    // else {
+    //     echo "No questions fetched!";
+    // }
+
+    echo "</body></htmL>";
+
+    $con->close();
+    echo "Closing Connection!";
+?>
