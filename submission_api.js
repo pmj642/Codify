6920a28a11995911736ca82c1b3b4f053c649e4c
Pranshu Jethmalani <pmj642@gmail.com>
2018-03-15 12:11:34 +0530

    Update submission_api.js

--- submission_api.js
+++ submission_api.js
@@ -36,9 +36,10 @@ function submit()
             if(xhr.readyState === XMLHttpRequest.DONE) {
                 console.log(xhr.response);
                 let outDiv = document.createElement('div');
+                outDiv.style.margin = 20px;
                 outDiv.innerHTML = "Time: " + xhr.response.time + "<br/>"
-                                    + "Memory" + xhr.response.memory + "<br/>"
-                                    + "Output" + xhr.response.stdout + "<br/>";
+                                    + "Memory: " + xhr.response.memory + "<br/>"
+                                    + "Output: " + xhr.response.stdout + "<br/>";
                 output.appendChild(outDiv);
               }
         };
