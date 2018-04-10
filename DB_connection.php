542b1bc6b982ce15a5eca03dc9d54045286cc183
Pranshu Jethmalani <pmj642@gmail.com>
2018-04-10 23:09:34 +0530

    cleaned connection file & added template code to practice.php

--- DB_connection.php
+++ DB_connection.php
@@ -13,31 +13,8 @@
     $con->set_charset("utf8");
     $sql = "select * from questions";
     $result = $con->query($sql);
-    // echo "Query successful<br>";
 
     $row = $result->fetch_assoc();
-    echo "<html>
-            <head>
-                <title>".$row["name"]."</title>
-                <meta charset='utf-8'>
-            </head>
-            <body>";
-    // if($result->num_rows>0) {
-        // while() {
-            echo "name: ".$row["name"]."<br>";
-            $cons = $con->real_escape_string($row["constraints"]);
-
-            // nl - newline br - break
-            echo nl2br($cons)."<br>";
-            
-        // }
-    // }
-    // else {
-    //     echo "No questions fetched!";
-    // }
-
-    echo "</body></htmL>";
-
+    
     $con->close();
-    echo "Closing Connection!";
 ?>
--- question.php
+++ question.php
@@ -7,29 +7,14 @@
             <script type="text/javascript" src="submission_api.js">
             </script>
         </head>
-        
-        <body>
 
-            <!-- Header -->
+        <script type="text/javascript" src="assets/scripts/getTemplate.js">    </script>
 
-            <header class="container group">
+    	<body onload="getTemplate()">
 
-                <h1 class="logo">
-                    <a href="index.html">Codify</a>
-                </h1>
+    		<!-- Header -->
 
-                <h3 class="tagline">Turn ideas into Code !</h3>
-
-                <nav class="nav primary-nav">
-                    <ul>
-                        <li>    <a href="index.html">Home</a>   </li><!--
-                        --><li>    <a href="practice.html">Practice</a>   </li><!--
-                        --><li>    <a href="register.html">Register</a>   </li><!--
-                        --><li>    <a href="rankings.html">Rankings</a>   </li>
-                    </ul>
-                </nav>
-
-            </header>
+            <header id="header" class="container group">        </header>
 
             <section class="row">
               <div class="grid">
@@ -38,54 +23,56 @@
 
                 <section class="col-2-3">
 
-                        <?php
-                            $con = new mysqli("localhost","root","","oj");
-                            if($con->connect_error)
-                            {
-                                die("Failed to connect to database! ".$con->connect_error);
-                            }
+                    <!-- script to fetch the question list -->
+                    
+                    <?php
+                        $con = new mysqli("localhost","root","","oj");
+                        if($con->connect_error)
+                        {
+                            die("Failed to connect to database! ".$con->connect_error);
+                        }
 
-                            // echo "Connected to database successfully<br>";
-                            // echo "Query successful";
+                        // echo "Connected to database successfully<br>";
+                        // echo "Query successful";
 
-                            $color = "color: #000;";
-                            $div = "//background: #f8f9f9;
-                                    //border: 1px solid #000;
-                                    padding: 30px;
-                                    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),
-                                                0 6px 20px 0 rgba(0,0,0,0.2);
-                                    ";
+                        $color = "color: #000;";
+                        $div = "//background: #f8f9f9;
+                                //border: 1px solid #000;
+                                padding: 30px;
+                                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),
+                                            0 6px 20px 0 rgba(0,0,0,0.2);
+                                ";
 
-                            $con->set_charset("utf8");
-                            $sql = "select * from questions";
-                            $result = $con->query($sql);
-                            // echo "Query successful<br>";
+                        $con->set_charset("utf8");
+                        $sql = "select * from questions";
+                        $result = $con->query($sql);
+                        // echo "Query successful<br>";
 
-                            $row = $result->fetch_assoc();
+                        $row = $result->fetch_assoc();
 
-                            echo "<div style = '$div'>";
-                            echo "<h1 style = '$color'>".$row["name"]."</h1>";
-                            echo "<p style = '$color'>".$row["description"]."</p>";
+                        echo "<div style = '$div'>";
+                        echo "<h1 style = '$color'>".$row["name"]."</h1>";
+                        echo "<p style = '$color'>".$row["description"]."</p>";
 
-                            echo "<h2 style = '$color'>Input</h2>";
-                            echo "<p style = '$color'>".$row["inputFormat"]."</p>";
+                        echo "<h2 style = '$color'>Input</h2>";
+                        echo "<p style = '$color'>".$row["inputFormat"]."</p>";
 
-                            echo "<h2 style = '$color'>Output</h2>";
-                            echo "<p style = '$color'>".$row["outputFormat"]."</p>";
+                        echo "<h2 style = '$color'>Output</h2>";
+                        echo "<p style = '$color'>".$row["outputFormat"]."</p>";
 
-                            echo "<h2 style = '$color'>Constraints</h2>";
-                            echo "<p style = '$color'>".nl2br($row["constraints"])."</p>";
+                        echo "<h2 style = '$color'>Constraints</h2>";
+                        echo "<p style = '$color'>".nl2br($row["constraints"])."</p>";
 
-                            echo "<h2 style = '$color'>Example</h2>";
-                            echo "<h3 style = '$color'>Input</h3>";
-                            echo "<p style = '$color'>".nl2br($row["exampleIn"])."</p>";
-                            echo "<h3 style = '$color'>Output</h3>";
-                            echo "<p style = '$color'>".nl2br($row["exampleOut"])."</p>";
-                            echo "</div>";
+                        echo "<h2 style = '$color'>Example</h2>";
+                        echo "<h3 style = '$color'>Input</h3>";
+                        echo "<p style = '$color'>".nl2br($row["exampleIn"])."</p>";
+                        echo "<h3 style = '$color'>Output</h3>";
+                        echo "<p style = '$color'>".nl2br($row["exampleOut"])."</p>";
+                        echo "</div>";
 
-                            $con->close();
-                            // echo "Closing Connection!";
-                        ?>
+                        $con->close();
+                        // echo "Closing Connection!";
+                    ?>
                     <!-- </div> -->
                 </section><!--
 
@@ -108,20 +95,9 @@
               </div>
             </section>
 
-            <footer class="primary-footer container group">
+            <!-- Footer -->
 
-                <small>&copy;Codify</small>
-
-                <nav class="nav">
-                    <ul>
-                        <li>    <a href="index.html">Home</a>   </li>
-                        <li>    <a href="practice.html">Practice</a>   </li>
-                        <li>    <a href="register.html">Register</a>   </li>
-                        <li>    <a href="rankings.html">Rankings</a>   </li>
-                    </ul>
-                </nav>
-
-            </footer>
+    		<footer id="footer" class="primary-footer container group">        </footer>
 
         </body>
 </html>
