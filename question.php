a0fa4e37ad25e2f569ed1e17dc811e56bee26035
Pranshu Jethmalani <pmj642@gmail.com>
2018-05-14 17:34:21 +0530

    removed nl2br

--- question.php
+++ question.php
@@ -66,14 +66,16 @@
                         echo "<h2 style = '$color'>Output</h2>";
                         echo "<p style = '$color'>".$row["outputformat"]."</p>";
 
+                        // add nl2br for mysql nl2br($row["constraints"])
+
                         echo "<h2 style = '$color'>Constraints</h2>";
-                        echo "<p style = '$color'>".nl2br($row["constraints"])."</p>";
+                        echo "<p style = '$color'>".$row["constraints"]."</p>";
 
                         echo "<h2 style = '$color'>Example</h2>";
                         echo "<h3 style = '$color'>Input</h3>";
-                        echo "<p style = '$color'>".nl2br($row["examplein"])."</p>";
+                        echo "<p style = '$color'>".$row["examplein"]."</p>";
                         echo "<h3 style = '$color'>Output</h3>";
-                        echo "<p style = '$color'>".nl2br($row["exampleout"])."</p>";
+                        echo "<p style = '$color'>".$row["exampleout"]."</p>";
                         echo "</div>";
 
                         $con->close();
