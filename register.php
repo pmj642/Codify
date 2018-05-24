428948752fa19e2bf8af0f923dd02b9e97ec8919
Pranshu Jethmalani <pmj642@gmail.com>
2018-05-24 20:53:56 +0530

    resolving bug still

---
+++ register.php
@@ -0,0 +1,13 @@
+<?php
+    // $name = $_POST["name"];
+    // $user = $_POST["user"];
+    // $pass = $_POST["pass"];
+    // $age = $_POST["age"];
+    // $gender = $_POST["gender"];
+    // $email = $_POST["email"];
+    // $country = $_POST["country"];
+
+    // echo $name." ".$user." ".$pass;
+
+    echo "Hwllo";
+?>
