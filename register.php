6709562edb41615cce1f28b8f45e378118793979
Pranshu Jethmalani <pmj642@gmail.com>
2018-05-27 18:30:49 +0530

    testing register.php

--- register.php
+++ register.php
@@ -14,23 +14,24 @@
 
     if(!$con)
     {
+        echo "Failed to connect!";
         die("Failed to connect to database!");
     }
 
     // check for duplicate username and show error
 
-    $sql = "select * from userlogin where user=" + $email;
-    $result = pg_query($sql);
-
-    if(pg_num_rows($result))
-    {
-        session_start();
-        $_SESSION["msg"] = 'Email already exists!';
-        header(Location: 'register_form.php');
-    }
-
-    // hash the Password
-
-    $hashPass = password_hash($pass, PASSWORD_DEFAULT);
-    echo $hashPass;
+    // $sql = "select * from userlogin where user=" + $email;
+    // $result = pg_query($sql);
+    //
+    // if(pg_num_rows($result))
+    // {
+    //     session_start();
+    //     $_SESSION["msg"] = 'Email already exists!';
+    //     header(Location: 'register_form.php');
+    // }
+    //
+    // // hash the Password
+    //
+    // $hashPass = password_hash($pass, PASSWORD_DEFAULT);
+    // echo $hashPass;
 ?>
