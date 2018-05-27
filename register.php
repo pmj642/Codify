c847c304ebaf04aaa8a14cda10b682c099d35488
Pranshu Jethmalani <pmj642@gmail.com>
2018-05-27 18:28:23 +0530

    testing register.php

--- register.php
+++ register.php
@@ -1,5 +1,4 @@
 <?php
-    // session_start();
 
     $name = $_POST["name"];
     $pass = $_POST["pass"];
@@ -8,29 +7,30 @@
     $email = $_POST["email"];
     $country = $_POST["country"];
 
-    echo $name." ".$user." ".$pass;
-    echo "Hello";
+    // echo $name." ".$user." ".$pass;
+    // echo "Hello";
 
-    // $con = pg_connect(getenv("DATABASE_URL"));
-    //
-    // if(!$con)
-    // {
-    //     die("Failed to connect to database!");
-    // }
-    //
-    // // check for duplicate username and show error
-    //
-    // $sql = "select * from userlogin where user=" + $email;
-    // $result = pg_query($sql);
-    //
-    // if(pg_num_rows($result))
-    // {
-    //     $_SESSION["msg"] = 'Email already exists!';
-    //     header(Location: 'register_form.php');
-    // }
-    //
-    // // hash the Password
-    //
-    // $hashPass = password_hash($pass, PASSWORD_DEFAULT);
-    // echo $hashPass;
+    $con = pg_connect(getenv("DATABASE_URL"));
+
+    if(!$con)
+    {
+        die("Failed to connect to database!");
+    }
+
+    // check for duplicate username and show error
+
+    $sql = "select * from userlogin where user=" + $email;
+    $result = pg_query($sql);
+
+    if(pg_num_rows($result))
+    {
+        session_start();
+        $_SESSION["msg"] = 'Email already exists!';
+        header(Location: 'register_form.php');
+    }
+
+    // hash the Password
+
+    $hashPass = password_hash($pass, PASSWORD_DEFAULT);
+    echo $hashPass;
 ?>
