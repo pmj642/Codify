6d8f25c38d17602fef26f155d7665a5bffddb54c
Pranshu Jethmalani <pmj642@gmail.com>
2018-05-30 14:01:38 +0530

    replaced footer.html -> footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer id="footer" class="primary-footer container group">
+
+    <small>Codify powered by</small>
+
+    <nav class="nav">
+        <ul>
+            <li>    <a href="index.html">Home</a>          </li>
+            <li>    <a href="question.php">Practice</a>   </li>
+            <li>    <a href="register_form.php">Register</a>   </li>
+            <li>    <a href="rankings.html">Rankings</a>   </li>
+            <li>    <a href="submit_test.html">Test</a>   </li>
+        </ul>
+    </nav>
+
+    <a href="https://judge0.com/"> <img src="assets/images/judge_logo_white.png"/> </a>
+    
+</footer>
