a3e74822350d0d11ef7874700e0fc790b01e58cb
Pranshu Jethmalani <pmj642@gmail.com>
2018-05-31 13:49:21 +0530

    added logout.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+
+        session_start();
+        session_destroy();
+
+        $r = $_SERVER['HTTP_REFERER'];
+
+        if(isset($r))
+            header('Location: '.$r);
+        else
+            header('Location: index.php');
+
+?>
