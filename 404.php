9ea03f4736762574380c65b582f51c672126e3c6
Pranshu Jethmalani <pmj642@gmail.com>
2018-06-02 18:13:54 +0530

    added 404 error page

---
+++ 404.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <title>Page not found</title>
+            <meta charset='utf-8'>
+            <link rel="stylesheet" href="assets/stylesheets/main.css">
+        </head>
+
+    	<body>
+
+    		<!-- Header -->
+
+            <?php
+                require('header.php');
+            ?>
+
+            <section class="row">
+              <div class="grid">
+
+                <!-- Question -->
+
+                <section class="card black-heading not-found">
+
+                    <h1>404</h1>
+
+                    <br>
+                    <h2>Page not found</h2>
+
+                    <img src='assets/images/broken-link.png'>
+                    
+                </section>
+
+              </div>
+            </section>
+
+            <!-- Footer -->
+
+            <?php
+                require('footer.php');
+            ?>
+
+        </body>
+</html>
