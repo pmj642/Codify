0ef2faf187846a655afa1db4c38e0b313de828d7
Pranshu Jethmalani <pmj642@gmail.com>
2018-11-20 10:14:35 +0530

    added code for submission from server

--- app/submit.php
+++ app/submit.php
@@ -1,6 +1,28 @@
 <?php
+    require '../vendor/autoload.php' ;
+
     $req = file_get_contents('php://input');
+    $req = json_decode($req);
 
     header('Content-type: application/json');
-    echo $req;
+    // echo "THIS IS REQUEST\n";
+    // echo $req;
+
+    $apiUrl = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
+
+    use GuzzleHttp\Client;
+
+    $client = new Client(['base_uri' => $apiUrl]);
+
+    // echo "source_code"." ".$req->{'source_code'};
+    // echo "language_id"." ".$req->{'language_id'};
+
+    $result = $client -> request('POST', $apiUrl, ['json' => [
+        'source_code' => $req->{'source_code'},
+        'language_id' => $req->{'language_id'}
+    ]]);
+
+    echo "THIS IS RESULT\n";
+    echo $result->getStatusCode();
+    echo $result->getBody();
 ?>
