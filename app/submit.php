77dad493c8acfee32aed22891d14c2ed2e98522f
Pranshu J <pmj642@gmail.com>
2018-11-21 23:12:03 +0530

    submissions succesfully saved to database w/o testcase support

--- app/submit.php
+++ app/submit.php
@@ -1,34 +1,74 @@
 <?php
-    require '../vendor/autoload.php' ;
+
+    require '../vendor/autoload.php';
     use GuzzleHttp\Client;
 
-    date_default_timezone_set('UTC');
-    $ctime = date('Y/m/d H:i:s');
+    //  check if user has logged in
 
-    $req = file_get_contents('php://input');
-    $req = json_decode($req);
+    if(!isset($_SESSION))
+        session_start();
 
-    header('Content-type: application/json');
+    if(isset($_SESSION["user_id"]))
+    {
+        $userId = $_SESSION["user_id"];
 
-    $apiUrl = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
+        date_default_timezone_set('UTC');
+        $ctime = date('Y/m/d H:i:s');
 
-    $client = new Client(['base_uri' => $apiUrl]);
+        $req = file_get_contents('php://input');
+        $req = json_decode($req);
 
-    $result = $client -> request('POST', $apiUrl, ['json' => [
-        'source_code' => $req->source_code,
-        'language_id' => $req->language_id
-    ]]);
+        header('Content-type: application/json');
 
-    echo "THIS IS RESULT\n";
-    echo $result->getStatusCode() . "\n";
+        $apiUrl = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
 
-    $response = json_decode($result->getBody());
+        $client = new Client(['base_uri' => $apiUrl]);
 
-    echo $response->time . "\n";
-    echo $response->status->description . "\n";
-    echo $ctime . "\n";
+        $result = $client -> request('POST', $apiUrl, ['json' => [
+            'source_code' => $req->source_code,
+            'language_id' => $req->language_id
+        ]]);
 
-//  make submission entry in database
+        echo "THIS IS RESULT\n";
+        echo $result->getStatusCode() . "\n";
 
+        $response = json_decode($result->getBody());
+
+        echo $response->time . "\n";
+        echo $response->status->description . "\n";
+        echo $ctime . "\n";
+        echo $_SESSION["user_id"] . "\n";
+
+        // make submission entry in database
+
+        $con = new mysqli("localhost","root","","oj");
+
+        if($con->connect_error)
+        {
+            die("Failed to connect to database! <br> Error:".$con->connect_error);
+        }
+
+        $responseTime = $response->time;
+        $responseStatus = $response->status->description;
+        $questionId = $req->question_id;
+
+        $sql = "insert into submissions values(
+                '$ctime','$userId','$questionId','$responseTime','$responseStatus')";
+
+        echo $sql . "\n";
+
+        $result = $con->query($sql);
+
+        echo $con -> error . "\n";
+
+        if($result)
+            echo "Succesful submission!\n";
+        else
+            echo "Failed submission!\n";
+    }
+    else
+    {
+        echo "You need to login!";
+    }
 
 ?>
