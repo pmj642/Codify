0affda6d03b2c73bf41678b807b51a18e0e45015
Pranshu J <pmj642@gmail.com>
2018-11-22 13:43:32 +0530

    submissions are now fully working

--- app/submit.php
+++ app/submit.php
@@ -12,34 +12,12 @@
     {
         $userId = $_SESSION["user_id"];
 
-        date_default_timezone_set('UTC');
-        $ctime = date('Y/m/d H:i:s');
-
         $req = file_get_contents('php://input');
         $req = json_decode($req);
 
-        header('Content-type: application/json');
+        $questionId = $req->question_id;
 
-        $apiUrl = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
-
-        $client = new Client(['base_uri' => $apiUrl]);
-
-        $result = $client -> request('POST', $apiUrl, ['json' => [
-            'source_code' => $req->source_code,
-            'language_id' => $req->language_id
-        ]]);
-
-        echo "THIS IS RESULT\n";
-        echo $result->getStatusCode() . "\n";
-
-        $response = json_decode($result->getBody());
-
-        echo $response->time . "\n";
-        echo $response->status->description . "\n";
-        echo $ctime . "\n";
-        echo $_SESSION["user_id"] . "\n";
-
-        // make submission entry in database
+        // get testcases from database
 
         $con = new mysqli("localhost","root","","oj");
 
@@ -48,23 +26,70 @@
             die("Failed to connect to database! <br> Error:".$con->connect_error);
         }
 
-        $responseTime = $response->time;
-        $responseStatus = $response->status->description;
-        $questionId = $req->question_id;
-
-        $sql = "insert into submissions values(
-                '$ctime','$userId','$questionId','$responseTime','$responseStatus')";
+        $sql = "select * from testcases where que_id='$questionId'";
 
         echo $sql . "\n";
 
         $result = $con->query($sql);
 
-        echo $con -> error . "\n";
-
-        if($result)
-            echo "Succesful submission!\n";
+        if($result->num_rows == 0)
+        {
+            echo "Testcases not found!\n";
+        }
         else
-            echo "Failed submission!\n";
+        {
+            echo "Testcases found!\n";
+
+            $row = $result->fetch_assoc();
+
+            $input = $row["input"];
+            $output = $row["output"];
+
+            date_default_timezone_set('UTC');
+            $ctime = date('Y/m/d H:i:s');
+
+            header('Content-type: application/json');
+
+            $apiUrl = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
+
+            $client = new Client(['base_uri' => $apiUrl]);
+
+            $result = $client -> request('POST', $apiUrl, ['json' => [
+                'source_code' => $req->source_code,
+                'language_id' => $req->language_id,
+                'stdin' => $input,
+                'expected_output' => $output
+            ]]);
+
+            echo "THIS IS RESULT\n";
+            echo $result->getStatusCode() . "\n";
+
+            $response = json_decode($result->getBody());
+
+            echo $response->time . "\n";
+            echo $response->status->description . "\n";
+            echo $ctime . "\n";
+            echo $_SESSION["user_id"] . "\n";
+
+            // make submission entry in database
+
+            $responseTime = $response->time;
+            $responseStatus = $response->status->description;
+
+            $sql = "insert into submissions values(
+                    '$ctime','$userId','$questionId','$responseTime','$responseStatus')";
+
+            echo $sql . "\n";
+
+            $result = $con->query($sql);
+
+            echo $con -> error . "\n";
+
+            if($result)
+                echo "Succesful database entry!\n";
+            else
+                echo "Failed database entry!\n";
+        }
     }
     else
     {
