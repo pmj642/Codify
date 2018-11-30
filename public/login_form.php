45c8de8942c66cf444805f109ab76e9d8dce1f3c
Pranshu J <pmj642@gmail.com>
2018-11-30 14:30:24 +0530

    refined the css and structure for input forms

--- public/login_form.php
+++ public/login_form.php
@@ -19,41 +19,46 @@
 
 		<section class="row">
 			<div class="container">
-				<form method="post" action="../app/login.php">
 
-					<?php
+				<div class='centre card form-container'>
 
-						if(isset($_SESSION['user']))
-						{
-							header('Location: ../public/home.php');
-						}
+					<form method="post" action="../app/login.php">
 
-						if(isset($_SESSION['successMsg']))
-						{
-							echo "<p class='reporting success'>".$_SESSION['successMsg']."</p>";
-							unset($_SESSION['msg']);
-						}
+						<?php
 
-						if(isset($_SESSION['errorMsg']))
-						{
-							echo "<p class='reporting error'>".$_SESSION['errorMsg']."</p>";
-							unset($_SESSION['errorMsg']);
-						}
-					?>
+							if(isset($_SESSION['user']))
+							{
+								header('Location: ../public/home.php');
+							}
 
-					<!-- <p class="reporting error">Username already exists!</p>
-					<p class="reporting success">User created!</p> -->
+							if(isset($_SESSION['successMsg']))
+							{
+								echo "<p class='reporting success'>".$_SESSION['successMsg']."</p>";
+								unset($_SESSION['msg']);
+							}
 
-					Email
-					<input type="email" name="email" placeholder="Your email..." required>
+							if(isset($_SESSION['errorMsg']))
+							{
+								echo "<p class='reporting error'>".$_SESSION['errorMsg']."</p>";
+								unset($_SESSION['errorMsg']);
+							}
+						?>
 
-					<br>Password
-					<input type="password" name="pass" placeholder="Password..." required>
+						<!-- <p class="reporting error">Username already exists!</p>
+						<p class="reporting success">User created!</p> -->
 
-					<br>
-					<input type="submit" value="Log-in">
+						Email
+						<input type="email" name="email" placeholder="Your email..." required>
 
-				</form>
+						<br>Password
+						<input type="password" name="pass" placeholder="Password..." required>
+
+						<br>
+						<input type="submit" value="Log-in">
+
+					</form>
+
+				</div>
 
 			</div>
 		</section>
--- public/register_form.php
+++ public/register_form.php
@@ -19,60 +19,65 @@
 
 		<section class="row">
 			<div class="container">
-				<form method="post" action="../app/register.php">
 
-					<?php
+				<div class='centre card form-container'>
 
-						if(isset($_SESSION['user']))
-						{
-							header('Location: ../public/home.php');
-						}
+					<form method="post" action="../app/register.php">
 
-						if(isset($_SESSION['errorMsg']))
-						{
-							echo "<p class='reporting error'>".$_SESSION['errorMsg']."</p>";
-							unset($_SESSION['msg']);
-						}
+						<?php
 
-					?>
+							if(isset($_SESSION['user']))
+							{
+								header('Location: ../public/home.php');
+							}
 
-					<!-- <p class="reporting error">Username already exists!</p>
-					<p class="reporting success">User created!</p> -->
+							if(isset($_SESSION['errorMsg']))
+							{
+								echo "<p class='reporting error'>".$_SESSION['errorMsg']."</p>";
+								unset($_SESSION['msg']);
+							}
 
-					Name
-					<input type="text" name="name" placeholder="Your name..." required>
+						?>
 
-					<br>Email
-					<input type="email" name="email" placeholder="Your email..." required>
+						<!-- <p class="reporting error">Username already exists!</p>
+						<p class="reporting success">User created!</p> -->
 
-					<br>Password
-					<input type="password" name="pass" placeholder="Password..." required>
+						Name
+						<input type="text" name="name" placeholder="Your name..." required>
 
-					<br>Age
-					<input type="number" name="age" min="5" max="100" placeholder="Your age..."required>
+						<br>Email
+						<input type="email" name="email" placeholder="Your email..." required>
 
-					<br>Gender
-						<select name="gender" required>
+						<br>Password
+						<input type="password" name="pass" placeholder="Password..." required>
 
-							<option value="male">Male</option>
-							<option value="female">Female</option>
-							<option value="other">Other</option>
+						<br>Age
+						<input type="number" name="age" min="5" max="100" placeholder="Your age..."required>
 
-						</select>
+						<br>Gender
+							<select name="gender" required>
 
-					<br>Country
-						<select name="country" required>
+								<option value="male">Male</option>
+								<option value="female">Female</option>
+								<option value="other">Other</option>
 
-							<option value="india">India</option>
-							<option value="china">China</option>
-							<option value="russia">Russia</option>
+							</select>
 
-						</select>
+						<br>Country
+							<select name="country" required>
 
-					<br>
-					<input type="submit" value="Register">
+								<option value="india">India</option>
+								<option value="china">China</option>
+								<option value="russia">Russia</option>
 
-				</form>
+							</select>
+
+						<br>
+						<input type="submit" value="Register">
+
+					</form>
+
+				</div>
 
 			</div>
 		</section>
