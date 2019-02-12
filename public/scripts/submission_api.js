0f8ec70c04b4f54da29b17f7b2f340e41a93ebd4
Pranshu J <pmj642@gmail.com>
2019-02-12 18:35:14 +0530

    update submission functionality after judge0 api change on ide page

--- public/scripts/submission_api.js
+++ public/scripts/submission_api.js
@@ -2,8 +2,6 @@
 
 function displayResult(responseObj)
 {
-    console.log("RESPONSE\n" + responseObj);
-
     let result = document.getElementById('result');
     let verdict = document.getElementById('verdict');
     let resultBox = document.getElementsByClassName('result-box');
@@ -22,7 +20,7 @@ function displayResult(responseObj)
     {
         result.innerHTML =    "<img src='../public/images/clock-icon.png'>" + responseObj.time + " sec<br/>"
                             + "<img src='../public/images/memory-icon.png'>" + responseObj.memory + " kB<br/>"
-                            + "<img src='../public/images/output-icon.png'>" + responseObj.stdout;
+                            + "<img src='../public/images/output-icon.png'>" + responseObj.stdout.substr(0,10) + "...";
 
         verdict.style.width = result.style.width = "50%";
     }
@@ -57,9 +55,6 @@ function submit()
     }
 
     reader.onload = function () {
-        let xhr = new XMLHttpRequest();
-        let url = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
-
         let request = {
             source_code: reader.result,
             language_id: langType,
@@ -69,18 +64,41 @@ function submit()
         if(expected_out.value)
             request.expected_output = expected_out.value;
 
-        xhr.responseType = 'json';
+        // get token for submission
+        let xhrToken = new XMLHttpRequest();
+        xhrToken.open("POST", "https://api.judge0.com/submissions/?base64_encoded=false&wait=false");
+        xhrToken.responseType = 'json';
+        xhrToken.setRequestHeader("Content-type", "application/json");
+        xhrToken.send(JSON.stringify(request));
 
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === XMLHttpRequest.DONE) {
-                displayResult(xhr.response);
+        xhrToken.onreadystatechange = function() {
+            if(xhrToken.readyState === XMLHttpRequest.DONE) {
+                console.log(xhrToken.response);
+
+                // get submission result
+                let submissionToken = xhrToken.response.token;
+                let xhr = new XMLHttpRequest();
+                xhr.responseType = 'json';
+                xhr.open("GET", "https://api.judge0.com/submissions/" + submissionToken + "?fields=stdout,stderr,status,memory,time");
+                xhr.setRequestHeader("Content-type", "application/json");
+                xhr.send();
+
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState === XMLHttpRequest.DONE) {
+                        console.log(xhr.response);
+
+                        if(xhr.response.status.description === "Processing" || xhr.response.status.description === "In Queue")
+                            setTimeout(function() {
+                                xhr.open("GET", "https://api.judge0.com/submissions/" + submissionToken + "?fields=stdout,stderr,status,memory,time");
+                                xhr.setRequestHeader("Content-type", "application/json");
+                                xhr.send();
+                            }, 1000);
+                        else
+                            displayResult(xhr.response);
+                    }
+                };
             }
         };
-
-        xhr.open("POST",url);
-        xhr.setRequestHeader("Content-type", "application/json");
-
-        xhr.send(JSON.stringify(request));
     };
 
     reader.readAsText(file);
